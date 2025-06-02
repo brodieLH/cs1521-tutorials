866f89329e10d56282f8cb09de4bbcef6b15c101
brodie-h <halesbrodie@gmail.com>
2025-06-02 15:49:35 +1000

    added print new line

--- week1/add.s
+++ week1/add.s
@@ -1,15 +1,20 @@
 .text
 
 main: 
-	li $t0, 12
-	li $t1, 30
+	li      $t0, 12
+	li      $t1, 30
 
-	add $t2, $t0, $t1  # Add the two numbers
+	add     $t2, $t0, $t1 # Add the two numbers
 
-	li $v0, 1          # Syscall value for print integer
+	li      $v0, 1        # Syscall value for print integer
 
-	move $a0, $t2      # Move the result to $a0 as print int syscall expects it there
-	syscall	    	   # Run syscall, that currently exists in $v0
+	move    $a0, $t2      # place result in a0 
+                          # as print int syscall expects it there
+	syscall	    	      # execute syscall, that is in $v0
 
-	li $v0, 0
-	jr $ra      	   # return 0 from main
+    li      $a0, '\n'
+    li      $v0, 11       # Syscall value for print character
+    syscall             
+
+	li      $v0, 0
+	jr      $ra      	   # return 0 from main
