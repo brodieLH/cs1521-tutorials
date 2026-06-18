d5e6459537fdd1492b38795208aa4b3855878c71
brodie-h <halesbrodie@gmail.com>
2026-06-18 10:40:50 +1000

    added small sw example to week3

---
+++ week03/examples/example.s
@@ -0,0 +1,14 @@
+    .text
+main:
+	li	$t1, 42
+
+	# get address
+
+	# calculate offset
+	# i * size of value (int = 4, char = 1)
+
+	# add address and offset 
+
+	# store 42 at new address we calculated
+    .data
+test: 	.word 1, 2, 3, 4
